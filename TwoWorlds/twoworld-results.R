31d9d0992eb3a84e9734e24f3512b35781e11643
hejtmy <hejtmy@gmail.com>
2018-02-02 18:35:23 -0800

    Adds basic computation of sop results

--- TwoWorlds/twoworld-results.R
+++ TwoWorlds/twoworld-results.R
@@ -1,9 +1,8 @@
 sop_trial_pointing <- function(obj, trialId){
-  ls_pos <- get_trial_start_point_pos(obj, trialId)
+  ls_pos <- get_trial_start_goal(obj, trialId)
   #when player points, trial ends so the point is at the trial end
-  dt_player <- get_player_log_trial(obj, trialId)
-  point_line <- tail(dt_player, 1)
+  point_line <- get_trial_point.sop(obj, trialId)
   ls <- as.list(get_rotations(point_line))
-  ls$correct_angle <- brainvr.R::angle_from_positions(ls_pos$start, ls_pos$point)
+  ls$correct_angle <- navr::angle_from_positions(ls_pos$start, ls_pos$goal)
   return(ls)
 }
