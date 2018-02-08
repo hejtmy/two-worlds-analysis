7933e341ecb2fc581645e8e640f56c17cc631881
hejtmy <hejtmy@gmail.com>
2018-02-08 00:47:12 -0800

    Changes from building to mere unloading

--- build.R
+++ build.R
@@ -1,8 +1,8 @@
 detach("package:brainvr.R", unload = TRUE)
 detach("package:restimoter", unload = TRUE)
-
-install.packages("../brainvr-reader/",  type = "source", repos = NULL)
-install.packages("../restimoter/", type = "source", repos = NULL )
+detach("package:navr", unload = TRUE)
+#install.packages("../brainvr-reader/",  type = "source", repos = NULL)
+#install.packages("../restimoter/", type = "source", repos = NULL )
 
 library(brainvr.R)
 library(restimoter)
