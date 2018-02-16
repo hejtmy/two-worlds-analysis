9f7eeab20fecc05331716fd081e06a8ed350bc34
hejtmy <hejtmy@gmail.com>
2018-02-15 23:13:44 -0800

    MOves code into generics. Fixes some previous errors

--- TwoWorlds/twoworld-results.R
+++ TwoWorlds/twoworld-results.R
@@ -4,45 +4,44 @@ source("TwoWorlds/helpers-results.R")
 sop_results <- function(obj){
   UseMethod("sop_results")
 }
-sop_trial_results <- function(obj){
+sop_trial_results <- function(obj, trialId){
   UseMethod("sop_trial_results")
 }
 learn_results <- function(obj){
   UseMethod("learn_results")
 }
-learn_trial_results <- function(obj){
+learn_trial_results <- function(obj, trialId){
   UseMethod("learn_results")
 }
 ## UNITY ----
 sop_results.twunity <- function(obj){
-  df <- create_sop_df()
-  for (iTrial in 1:N_POINTING){
-    point <- sop_trial_pointing(obj, iTrial)
-    times <- get_trial_times(obj, iTrial)
-    df[iTrial, "pointing_time"] <- times$finish - times$start
-    df[iTrial, "pointing_error"] <- point$pointing_error
+  df <- create_sop_df(obj$sop)
+  for (trialId in 1:12){
+    ls <- sop_trial_results.twunity(obj, trialId)
+    df[trialId, "pointing_time"] <- ls$pointing_time
+    df[trialId, "pointing_error"] <- ls$pointing_error
   }
   return(df)
 }
 sop_trial_results.twunity <- function(obj, trialId){
   sop <- obj$sop
-  # WEIRD thing that the controller actually was transformed by this amout, so we need to input it back
-  UNITY_SHIFT <- -90
+  UNITY_SHIFT <- -90 # WEIRD thing that the controller actually was transformed by this amout, so we need to input it back
   ls_pos <- get_trial_start_goal(sop, trialId)
-  #when player points, trial ends so the point is at the trial end
   point_line <- get_trial_point.sop(sop, trialId)
   ls <- as.list(get_rotations(point_line))
   ls$pointed_angle <- navr::angle_to_360(point_line$Rotation.Controller.x) + UNITY_SHIFT
   ls$correct_angle <- navr::angle_from_positions(ls_pos$start, ls_pos$goal)
   ls$pointing_error <- navr::angle_to_180(ls$correct_angle - ls$pointed_angle)
+  times <- get_trial_times(sop, trialId)
+  ls$pointing_time <- times$finish - times$start
   return(ls)
 }
 learn_results.twunity <- function(obj){
   df_results <- create_learn_df(obj$learn)
-  for (iTrial in 1:18){
+  for (trialId in 1:18){
     ls <- learn_trial_results.twunity(obj)
-    df_results[iTrial, "trial_time"] <- ls$trial_time
-    df_results[iTrial, "trial_distance"] <- ls$trial_distance
+    df_results[trialId, "trial_time"] <- ls$trial_time
+    df_results[trialId, "trial_distance"] <- ls$trial_distance
   }
   return(df_results)
 }
@@ -55,22 +54,28 @@ learn_trial_results.twunity <- function(obj){
 }
 ## RESTIMOTE ----
 learn_results.restimote <- function(obj){
-  df_results <- create_learn_df()
-  for (iTrial in 1:18){
-    log <- get_trial_log(obj, iTrial)
-    log_true <- true_trial_log(obj, iTrial, 30, radius = 3)
-    df_results[iTrial, "trial_time"] <- diff(range(log$Time))
-    df_results[iTrial, "trial_distance"] <- sum(log_true$distance)
+  df_results <- create_learn_df(obj)
+  for (trialId in 1:18){
+    ls <- learn_trial_results.restimote(obj, trialId)
+    df_results[trialId, "trial_time"] <- ls$trial_time
+    df_results[trialId, "trial_distance"] <- ls$trial_distance
   }
   df_results$trial_distance[df_results$trial_distance == 0] <- NA
   return(df_results)
 }
+learn_trial_results.restimote <- function(obj, trialId){
+  log <- get_trial_log(obj, trialId)
+  log_true <- true_trial_log(obj, trialId, 30, radius = 3)
+  ls <- list()
+  ls$trial_time <- diff(range(log$Time))
+  ls$trial_distance <- sum(log_true$distance)
+}
 sop_results.restimote <- function(obj){
   df_results <- create_sop_df(obj)
-  for (iTrial in 1:N_POINTING){
-    sop_results.restimote(obj, trialId)
-    df[iTrial, "pointing_time"] <- ls$pointing_time
-    df[iTrial, "pointing_error"] <- point$pointing_error
+  for (trialId in 1:12){
+    ls <-  sop_results.restimote(obj, trialId)
+    df[trialId, "pointing_time"] <- ls$pointing_time
+    df[trialId, "pointing_error"] <- point$pointing_error
   }
   return(df)
 }
@@ -83,12 +88,4 @@ sop_trial_results.restimote <- function(obj, trialId){
   ls$pointing_error <- navr::angle_to_180(ls$correct_angle - ls$pointed_angle)
   ls$pointing_time <- times$end - times$start
   return(ls)
-}
-## GENERAL ----
-
-# Expects restimote object
-
-
-restimote_sop_trial_pointing <- function(obj, trialId){
-
 }
