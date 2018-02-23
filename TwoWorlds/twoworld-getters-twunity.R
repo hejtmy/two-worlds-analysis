3ed4704a45f9b9e85d3f4221064c2247daf35879
hejtmy <hejtmy@gmail.com>
2018-02-22 22:28:40 -0800

    SOP unity results

--- TwoWorlds/twoworld-getters-twunity.R
+++ TwoWorlds/twoworld-getters-twunity.R
@@ -1,78 +1,92 @@
-#X --get_trial_start_id
-#X --get_trial_start
-#X --get_trial_start_name
-#X --get_trial_goal_id
-#X --get_trial_goal
-#X --get_trial_goal_name
-#X --get_trial_start_goal
-#X --get_point_start_id
-#X --get_point_start_name
-#X --get_point_start
-#X --get_point_goal_id
-#X --get_point_goal_name
-#X --get_point_goal
-#X --get_trial_point
-#X --get_all_goal_positions
-### twunity ---------
-get_goal.brainvr <- function(obj, goal_id){
-  goal_pos <- obj$data$experiment_log$positions$GoalPositions[goal_id, ]
-  return(c(goal_pos$Position.x, goal_pos$Position.z))
+### twunity ----
+get_trial_start_id.twunity <- function(obj, trialId){
+  if(trialId == 1) return(NULL)
+  return(get_trial_goal_id.twunity(trialId - 1))
 }
-get_trial_goal.brainvr <- function(obj, trialId){
-  goal_id <- get_trial_goal_id(obj, trialId)
-  return(get_goal(obj, goal_id))
+get_trial_start.twunity <- function(obj, trialId){
+  #First trial is started in a center hall
+  iStart <- get_trial_start_id.twunity(obj, trialId)
+  if(is.null(iStart)){
+    df_firs_pos <- obj$walk$data$player_log[1, ]
+    return(c(df_firs_pos$Position.x[1], df_firs_pos$Position.z[1]))
+  }
+  return(get_goal.brainvr(obj$walk, iStart))
 }
-get_trial_goal_name.brainvr <- function(obj, trialId){
-  if(trialId == 0) return("Starting location")
-  id <- get_trial_goal_id(obj, trialId)
-  return(obj$data$experiment_log$settings$GoalNames[id])
+get_trial_start_name.twunity <- function(obj, trialId){
+  iStart <- get_trial_start_id.twunity(obj, trialId)
+  if(is.null(iStart)) return("Starting location")
+  return(get_goal_name.brainvr(obj$walk, iStart))
 }
-get_trial_start_goal.brainvr <- function(obj, trialId){
-  start <- get_trial_start(obj, trialId)
-  goal <- get_trial_goal(obj, trialId)
+get_trial_goal_id.twunity <- function(obj, trialId){
+  iGoal <- obj$walk$data$experiment_log$settings$GoalOrder[trialId]
+  return(iGoal + 1) #C# indexes from 0
+}
+get_trial_goal.twunity <- function(obj, trialId){
+  return(get_goal.brainvr(obj$walk, trialId))
+}
+get_trial_goal_name.twunity <- function(obj, trialId){
+  iGoal <- get_trial_goal_id.twunity(obj, trialId)
+  return(get_goal_name.brainvr(obj$walk, iGoal))
+}
+get_trial_start_goal.twunity <- function(obj, trialId){
+  start <- get_trial_start.twunity(obj, trialId)
+  goal <- get_trial_goal.twunity(obj, trialId)
   ls <- list(start = start, goal = goal)
   return(ls)
+}
+get_point_start_id.twunity <- function(obj, trialId){
+  iStart <- obj$sop$data$experiment_log$settings$GoalOrder[trialId]
+  return(iStart + 1)
+}
+get_point_start_name.twunity <- function(obj, trialId){
+  iStart <- get_point_start_id.twunity(obj, trialId)
+  return(paste0("Viewpoint ", iStart - 6))
+}
+get_point_start.twunity <- function(obj, trialId){
+  iStart <- get_point_start_id.twunity(obj, trialId)
+  return(get_goal.brainvr(obj$sop, iStart))
+}
+get_point_goal_id.twunity <- function(obj, trialId){
+  iGoal <- obj$sop$data$experiment_log$settings$GoalPointOrder[trialId]
+  return(iGoal + 1)
+}
+get_point_goal_name.twunity <- function(obj, trialId){
+  iGoal <- get_point_goal_id.twunity(obj, trialId)
+  return(get_goal_name.brainvr(obj$sop, iGoal))
+}
+get_point_goal.twunity <- function(obj, trialId){
+  iGoal <- get_point_goal_id.twunity(obj, trialId)
+  return(get_goal.brainvr(obj$sop, iGoal))
+}
+get_point_start_goal.twunity <- function(obj, trialId){
+  start <- get_point_start.twunity(obj, trialId)
+  goal <- get_point_goal.twunity(obj, trialId)
+  ls <- list(start = start, goal = goal)
+  return(ls)
+}
+get_trial_point.twunity <- function(obj, trialId){
+  #when player points, trial ends so the point is at the trial end, but because of unity timing
+  #sometimes logged after trial finishes
+  log <- get_trial_log(obj$sop, trialId)
+  point_line <- tail(log, 1)
+  return(point_line)
+}
+get_all_goal_positions.twunity <- function(obj, include_SOP){
+  return(get_all_goal_positions.brainvr(obj$sop, include_SOP))
+}
+### BrainVR ----
+get_goal.brainvr <- function(obj, iGoal){
+  goal_pos <- obj$data$experiment_log$positions$GoalPositions[iGoal, ]
+  return(c(goal_pos$Position.x, goal_pos$Position.z))
+}
+get_goal_name.brainvr <- function(obj, iGoal){
+  return(obj$data$experiment_log$settings$GoalNames[iGoal])
 }
 get_all_goal_positions.brainvr <- function(obj, include_SOP = FALSE){
   if(include_SOP){i <- 1:10}else{i <- 1:6}
   ls_goals <- setNames(split(obj$data$experiment_log$positions$GoalPositions[i, c(1,3)], i), i)
   return(ls_goals)
 }
-### walk ------
-get_trial_goal_id.walk <- function(obj, trialId){
-  # C# indexes from 0
-  goal_id <- obj$data$experiment_log$settings$GoalOrder[trialId]
-  return(goal_id + 1)
-}
-get_trial_start.walk <- function(obj, trialId){
-  #First trial is started in a center hall
-  if(trialId == 1){
-    df_firs_pos <- obj$data$player_log[1, ]
-    return(c(df_firs_pos$Position.x[1], df_firs_pos$Position.z[1]))
-  }
-  return(get_trial_goal(obj, trialId - 1))
-}
-## SOP ----
-get_trial_goal_id.sop <- function(obj, trialId){
-  #in SOP trials we actually care about Point order
-  goal_id <- obj$data$experiment_log$settings$GoalPointOrder[trialId]
-  #c# indexes from 0
-  return(goal_id + 1)
-}
-get_trial_start.sop <- function(obj, trialId){
-  goal_id <- obj$data$experiment_log$settings$GoalOrder[trialId] + 1
-  return(get_goal(obj, goal_id))
-}
-get_trial_start_id.sop <- function(obj, trialId){
-  return(get_trial_goal_id.walk(obj, trialId))
-}
-get_trial_point.sop <- function(obj, trialId){
-  #when player points, trial ends so the point is at the trial end, but because of unity timing
-  #sometimes logged after trial finishes
-  log <- get_trial_log(obj, trialId)
-  point_line <- tail(log, 1)
-  return(point_line)
-}
 get_point_start.sop <- function(obj, trialId){
-  return(get_trial_start_id.sop(obj, trialId))
+  
 }
--- TwoWorlds/twoworld-results.R
+++ TwoWorlds/twoworld-results.R
@@ -14,31 +14,11 @@ walk_trial_results <- function(obj, trialId){
   UseMethod("walk_trial_results")
 }
 ## UNITY ----
-sop_results.twunity <- function(obj){
-  return(sop_results.general(obj$sop))
-}
-sop_trial_results.twunity <- function(obj, trialId){
-  return(sop_trial_results(obj$sop))
-}
-sop_trial_results.sop <- function(obj, trialId){
-  UNITY_SHIFT <- -90 # WEIRD thing that the controller actually was transformed by this amout, so we need to input it back
-  ls_pos <- get_trial_start_goal(obj, trialId)
-  point_line <- get_trial_point.sop(obj, trialId)
-  ls <- as.list(get_rotations(point_line))
-  ls$pointed_angle <- navr::angle_to_360(point_line$Rotation.Controller.x) + UNITY_SHIFT
-  ls$correct_angle <- navr::angle_from_positions(ls_pos$start, ls_pos$goal)
-  ls$error <- navr::angle_to_180(ls$correct_angle - ls$pointed_angle)
-  times <- get_trial_times(obj, trialId)
-  ls$time <- times$finish - times$start
-  ls$start <- paste0("Viewpoint", get_trial_goal_id.sop())
-  ls$goal <- get_trial_goal_name.brainvr(obj, trialId)
-  return(ls)
-}
 walk_results.twunity <- function(obj){
   return(walk_results.general(obj$walk))
 }
 walk_trial_results.twunity <- function(obj, trialId){
- return(walk_trial_results.walk(obj$walk, trialId))
+  return(walk_trial_results.walk(obj$walk, trialId))
 }
 walk_trial_results.walk <- function(obj, trialId){
   ls <- list()
@@ -47,6 +27,24 @@ walk_trial_results.walk <- function(obj, trialId){
   ls$distance <- diff(range(log$cumulative_distance))
   ls$start <- get_trial_goal_name.brainvr(obj, trialId - 1)
   ls$goal <- get_trial_goal_name.brainvr(obj, trialId)
+  return(ls)
+}
+sop_results.twunity <- function(obj){
+  df_results <- create_sop_df(obj$sop)
+  return(sop_results.general(obj, df_results))
+}
+sop_trial_results.twunity <- function(obj, trialId){
+  UNITY_SHIFT <- -90 # WEIRD thing that the controller actually was transformed by this amout, so we need to input it back
+  ls_pos <- get_point_start_goal(obj, trialId)
+  point_line <- get_trial_point(obj, trialId)
+  ls <- as.list(get_rotations(point_line))
+  ls$pointed_angle <- navr::angle_to_360(point_line$Rotation.Controller.x) + UNITY_SHIFT
+  ls$correct_angle <- navr::angle_from_positions(ls_pos$start, ls_pos$goal)
+  ls$error <- navr::angle_to_180(ls$correct_angle - ls$pointed_angle)
+  times <- get_trial_times(obj$sop, trialId)
+  ls$time <- times$finish - times$start
+  ls$start <- get_point_start_name(obj, trialId)
+  ls$goal <- get_point_goal_name(obj, trialId)
   return(ls)
 }
 ## RESTIMOTE ----
@@ -64,7 +62,8 @@ walk_trial_results.restimote <- function(obj, trialId){
   return(ls)
 }
 sop_results.restimote <- function(obj){
-  return(sop_results.general(obj))
+  df_results <- create_sop_df(obj)
+  return(sop_results.general(obj, df_results))
 }
 sop_trial_results.restimote <- function(obj, trialId){
   ls <- list()
@@ -78,10 +77,8 @@ sop_trial_results.restimote <- function(obj, trialId){
   ls$goal <- get_point_goal_name.restimote(obj, trialId)
   return(ls)
 }
-
 # GENERAL ----
-sop_results.general <- function (obj){
-  df_results <- create_sop_df(obj)
+sop_results.general <- function (obj, df_results){
   for (trialId in 1:12){
     ls <- sop_trial_results(obj, trialId)
     df_results[trialId, "time"] <- ls$time
