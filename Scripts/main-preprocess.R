dc2eb3dc3cd25c4e17e1e1f907379c9782edf023
hejtmy <hejtmy@gmail.com>
2018-02-24 17:31:49 -0800

    Adds some preprocessing steps

--- Scripts/main-preprocess.R
+++ Scripts/main-preprocess.R
@@ -4,6 +4,7 @@ library(brainvr.R)
 library(restimoter)
 library(ggplot2)
 library(dplyr)
+library(reshape2)
 library(googlesheets)
 source("Scripts/loading.R")
 dir <- "D:/OneDrive/Vyzkum/Davis/Transfer/Data/"
@@ -15,8 +16,26 @@ sop_all <- multi_sop_results(ls)
 walk_all <- multi_walk_results(ls)
 
 conditions <- sop_all %>% select(id, type, phase) %>% unique() %>% group_by(id) %>% summarise(condition = paste(type, collapse = "-"))
+conditions$condition <- gsub('twunity', 'vr', conditions$condition)
+conditions$condition <- gsub('restimote', 'real', conditions$condition)
 sop_all <- right_join(sop_all, conditions, by = "id")
 walk_all <-  right_join(walk_all, conditions, by = "id")
+
+#normalisations
+distance_offices_summary <- walk_all %>% 
+  group_by(type, start, goal) %>% 
+  summarise(mean = mean(distance, na.rm = T), 
+           sd = sd(distance, na.rm = T), 
+           min = min(distance, na.rm = T), 
+           max = max(distance, na.rm = T)) %>%
+  arrange(start, goal)
+  
+wide_distance_offices_summary <- distance_offices_summary %>% melt(id.vars=c("type", "start", "goal")) %>% dcast(start + goal ~ type + variable)
+office_distance_type_diff_min <- distance_offices_summary %>% group_by(start, goal) %>% summarise(min_diff = diff(min))
+qplot(data = office_distance_type_diff_min, x = 1:nrow(office_distance_type_diff_min), y = min_diff, geom = "line")
+
+walk_all <- right_join(walk_all, distance_offices_summary[, c(1:3, 6)], by = c("type", "start", "goal"))
+walk_all$min_norm_distance <- walk_all$distance/walk_all$min
 
 write.table(sop_all, file = "sop.csv", sep=";")
 write.table(walk_all, file = "walk.csv", sep=";")
