6d0e725100809ecb1bf078beecf766da6df4054e
hejtmy <hejtmy@gmail.com>
2018-02-28 03:25:53 -0800

    Updates getting all doors

--- TwoWorlds/helpers-getters.R
+++ TwoWorlds/helpers-getters.R
@@ -7,6 +7,6 @@ get_rotations <- function(df){
 
 get_all_doors <- function(settings){
   i <- 1:nrow(settings$door_positions)
-  ls_doors <- setNames(split(settings$door_positions[i, 2:3], i), settings$door_positions[i, 1])
+  ls_doors <- setNames(split(settings$door_positions[i, c(2,3)], i), as.character(settings$door_positions[i, 1][[1]]))
   return(ls_doors)
 }
