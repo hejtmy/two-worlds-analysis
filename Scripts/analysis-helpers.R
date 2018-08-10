4dfeb409defe39a251933816a97fe21a0b83e3ec
hejtmy <hejtmy@gmail.com>
2018-08-10 04:05:35 +0200

    Adds arne's report

--- Scripts/analysis-helpers.R
+++ Scripts/analysis-helpers.R
@@ -1,6 +1,6 @@
 make_graph <- function(data, x_name, y_name, fill_name){
   ggplot(data, aes(x = data[, x_name], y = data[, y_name], fill=factor(data[, fill_name]))) +
-    stat_summary(fun.data=mean_cl_normal,position=position_dodge(0.95),geom="errorbar") + 
+    stat_summary(fun.data=mean_cl_normal,position=position_dodge(0.95), geom="errorbar") + 
     stat_summary(fun.y=mean,position=position_dodge(width=0.95), geom="bar") 
 }
 
@@ -8,4 +8,8 @@ make_line_graph <- function(data, x_name, y_name, fill_name){
   ggplot(data, aes(x = data[, x_name], y = data[, y_name], color=factor(data[, fill_name]))) +
     stat_summary(fun.data=mean_cl_normal,position=position_dodge(width=0.1), width = 0.2, geom="errorbar") + 
     stat_summary(fun.y=mean,position=position_identity(), size = 1, geom="line")
+}
+
+anova_table <- function(aov){
+  return(summary(aov)[[1]])
 }
