7a50cdcc73f8ae79d020d4ed27a39fc35db672c1
hejtmy <hejtmy@gmail.com>
2018-09-21 21:21:38 +0200

    Adds new helper methods

---
+++ TwoWorlds/helpers-report.R
@@ -0,0 +1,18 @@
+apa_p <- function(values){
+  output <- c()
+  for (value in values){
+    if(value < 0.01){ output <- c(output, "p < .001"); next}
+    if(value < 0.05) { output <- c(output, "p < .0.05"); next}
+    output <- c(output, paste0("p = ", round(value, 2)))
+  }
+  return(output)
+}
+block_t_test <- function(df, block1, block2, value, paired = T){
+  df <- df %>% filter(exp_block_id %in% c(block1,block2))
+  mean.na <- function(data){return(mean(data, na.rm = T))}
+  df_wide <- dcast(df, learning.condition + id ~ exp_block_id, value.var = value, fun.aggregate = mean.na)
+  colnames(df_wide) <- c("learning.condition","id", "block1","block2")
+  df_out <- df_wide %>% group_by(learning.condition) %>% do(tidy(t.test(.$block1, .$block2, paired = paired)))
+  df_out$p.value <- apa_p(df_out$p.value)
+  return(df_out)
+}
--- TwoWorlds/helpers-results.R
+++ TwoWorlds/helpers-results.R
@@ -1,9 +1,19 @@
 ez_prepare <- function(filtered_data, by){
-
   filtered_data <- filtered_data[complete.cases(filtered_data),]
   has_all <- filtered_data %>% group_by(id) %>% select(by) %>% count
   has_all <- has_all %>% filter(n > 4)
   filtered_data <- filtered_data %>% filter(id %in% has_all$id)
   filtered_data$id <- factor(filtered_data$id)
+  return(filtered_data)
+}
+
+
+ez_prepare_block <- function(df, blocks){
+  filtered_data <- df[df$exp_block_id %in% blocks, ]
+  filtered_data <- filtered_data[complete.cases(filtered_data),]
+  has_all <- filtered_data %>% group_by(id) %>% select(id, "exp_block_id") %>% unique() %>% count
+  has_all <- has_all %>% filter(n == 2)
+  filtered_data <- filtered_data %>% filter(id %in% has_all$id)
+  filtered_data$exp_block_id <- factor(filtered_data$exp_block_id)
   return(filtered_data)
 }
