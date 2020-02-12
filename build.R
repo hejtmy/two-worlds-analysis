2256d4daf54e29532e33d37d45ad9dfbaca03094
hejtmy <hejtmy@gmail.com>
2020-02-12 02:45:20 +0100

    removes build.R file

--- build.R
+++
@@ -1,8 +0,0 @@
-detach("package:brainvr.R", unload = TRUE)
-detach("package:restimoter", unload = TRUE)
-detach("package:navr", unload = TRUE)
-#install.packages("../brainvr-reader/",  type = "source", repos = NULL)
-#install.packages("../restimoter/", type = "source", repos = NULL )
-
-library(brainvr.R)
-library(restimoter)
